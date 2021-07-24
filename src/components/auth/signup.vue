c9364fad25662c73f2d0dd7368a52d4c141a8a06
subhadip1203 <subhadip1203@gmail.com>
2021-07-24 22:54:32 +0530

    first commit

---
+++ src/components/auth/signup.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="heading">Please enter details to Register</div>
+  <div class="loginForm">
+    <form>
+      <div class="formItems">
+        <div class="label">Name</div>
+        <input class="inputItem" type="text" />
+      </div>
+      <div class="formItems">
+        <div class="label">Email</div>
+        <input class="inputItem" type="email" />
+      </div>
+      <div class="formItems">
+        <div class="label">Password</div>
+        <div class="passDiv">
+          <input
+            class="inputItem"
+            :type="passwordVisible ? 'text' : 'password'"
+          />
+          <span class="viewPassword" @click="togglePasswordView">
+            <PasswordToggle />
+          </span>
+        </div>
+      </div>
+      <div>
+        <button class="submitButton" type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import PasswordToggle from "./passwordToggle.vue";
+export default {
+  name: "Signup",
+  components: {
+    PasswordToggle,
+  },
+  data() {
+    return {
+      passwordVisible: false,
+    };
+  },
+  methods: {
+    togglePasswordView() {
+      this.passwordVisible = !this.passwordVisible;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.heading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid #e4e4e4;
+}
+.loginForm {
+  width: 100%;
+}
+.formele {
+  padding: 20px;
+}
+.formItems {
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  padding: 10px;
+}
+
+.label {
+  text-align: left;
+  margin-bottom: 6px;
+}
+.inputItem {
+  width: 98%;
+  height: 30px;
+  font-size: 18px;
+}
+.passDiv {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+.viewPassword {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: -30px;
+}
+.submitButton {
+  width: 40%;
+  height: 30px;
+  float: right;
+  color: white;
+  background-color: #50b050;
+  border: 2px solid #50b050;
+  margin: 10px;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="container">
+    <div class="halfWidth">
+      <div class="company">Company Name</div>
+      <div class="heading">
+        <div
+          :class="authActive === 'login' ? 'activeDiv' : 'inactiveDiv'"
+          @click="changeAuthType('login')"
+        >
+          LOGIN
+        </div>
+        <div
+          :class="authActive === 'login' ? 'inactiveDiv' : 'activeDiv'"
+          @click="changeAuthType('signup')"
+        >
+          SIGNUP
+        </div>
+      </div>
+      <div class="login-or-signup" v-if="authActive === 'login'"><Login /></div>
+      <div class="login-or-signup" v-else><Signup /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from "@/components/auth/login.vue";
+import Signup from "@/components/auth/signup.vue";
+export default {
+  name: "Auth",
+  components: {
+    Login,
+    Signup,
+  },
+  data() {
+    return {
+      authActive: "login",
+    };
+  },
+  methods: {
+    changeAuthType(type) {
+      this.authActive = type;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+  background-color: #cccccc;
+}
+.halfWidth {
+  width: 50%;
+  height: fit-content;
+  background-color: white;
+}
+.company {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  width: 100%;
+  border-bottom: 1px solid grey;
+  background-color: azure;
+}
+.heading {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 40px;
+  cursor: pointer;
+}
+.inactiveDiv {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: grey;
+  color: white;
+  width: 50%;
+  height: 40px;
+  /* border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px; */
+}
+
+.activeDiv {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 50%;
+  height: 40px;
+}
+</style>
